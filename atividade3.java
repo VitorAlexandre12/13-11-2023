71f8f60293bf48885a6af6b9e81a2c9da0df42bf
VitorAlexandre12 <137965762+VitorAlexandre12@users.noreply.github.com>
2023-11-13 12:27:14 -0300

    Add files via upload

---
+++ atividade3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class atividade3 {
+    public static void main(String[] args) {
+        double kilo, fixo, precoKilo;
+        Scanner sc = new Scanner(System.in);
+        kilo = 5;
+        fixo = 48;
+        precoKilo = 48 / 5;
+        
+        System.out.println("O preço do kg é: " + precoKilo);
+    }
+}
